c01b12732026b0734f740116facf3ca0f7b17034
kimmomo8877 <kimmomo8877@gmail.com>
2021-03-07 21:53:23 +0900

    재활 예약 기능 추가

---
+++ app/src/main/java/com/example/and02/ui/sport/SportDetailFragment.java
@@ -0,0 +1,42 @@
+package com.example.and02.ui.sport;
+
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.navigation.Navigation;
+import com.example.and02.R;
+
+public class SportDetailFragment extends Fragment {
+    private Button btnToReservation;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            getArguments().getString("");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sport_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.btnToReservation = view.findViewById(R.id.btn_sportsDetail_toReservation);
+        this.btnToReservation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.navigation_sport_reservation);
+            }
+        });
+    }
+}
